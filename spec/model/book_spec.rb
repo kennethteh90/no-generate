a827781a73b2856732dfc5b426bd175c7332e88f
kennethteh90 <kennethteh90@gmail.com>
2017-10-24 09:55:58 +0800

    added model association tests

--- spec/model/book_spec.rb
+++ spec/model/book_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Book, type: :model do
 
   it { is_expected.to validate_presence_of(:name)}
   it { is_expected.to validate_presence_of(:author)}
+  it { is_expected.to have_many(:bookmarks) }
 
 end
--- spec/model/bookmark_spec.rb
+++ spec/model/bookmark_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Bookmark, type: :model do
 
   it { is_expected.to validate_presence_of(:page) }
   it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to belong_to(:book) }
 
 end
